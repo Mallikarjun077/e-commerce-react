6c06c65a3b93e78a8644460b15ec75ffb7cb3dee
mallikarjun <shirwalmallu@gmail.com>
2025-01-17 22:38:48 +0530

    second-commit

--- src/Pages/ShopCategory.jsx
+++ src/Pages/ShopCategory.jsx
@@ -1,27 +1,29 @@
-import React, { useContext } from 'react'
+import React, { useContext } from 'react';
 import "./ShopCategory.css"
-import { ShopContext } from '../Context/ShopContext'
-import dropdown_icon from '../assets/dropdown_icon.png'
-import Item from '../Items/Item'
+import { ShopContext } from '../Context/ShopContext';
+import dropdown_icon from '../assets/dropdown_icon.png';
+import Item from '../Items/Item';
 
 function ShopCategory(props) {
-  const {all_product}=useContext(ShopContext)
+  const { all_product } = useContext(ShopContext);
+  
+  // Filter products based on the selected category
+  const filteredProducts = all_product.filter(item => item.category === props.Category);
+  
   return (
     <div className='shop-category'>
-         <img src={props.banner} alt="" />
-         <div className="shopcategory-indexsort">
-          <p>
-            <span>Showing 1-12</span>out of 36 products
-          </p>
-          <div className="shopcategory-sort">
-            sort by <img src={dropdown_icon} alt="" />
-          </div>
-         </div>
-         <div className="shopcategory-products">
-       {
-        all_product.map((item,i)=>{
-          if(props.Category===props.item){
-            <Item
+      <img className='shopcategory-banner' src={props.banner} alt="" />
+      <div className="shopcategory-indexsort">
+        <p>
+          <span>Showing 1-{filteredProducts.length}</span> out of {all_product.length} products
+        </p>
+        <div className="shopcategory-sort">
+          sort by <img src={dropdown_icon}  alt="" />
+        </div>
+      </div>
+      <div className="shopcategory-products">
+        {filteredProducts.map(item => (
+          <Item
             key={item.id}
             id={item.id}
             name={item.name}
@@ -29,15 +31,10 @@ function ShopCategory(props) {
             new_price={item.new_price}
             old_price={item.old_price}
           />
-          }else{
-            null
-          }
-        })
-       }
-
-         </div>
+        ))}
+      </div>
     </div>
-  )
+  );
 }
 
-export default ShopCategory
+export default ShopCategory;
